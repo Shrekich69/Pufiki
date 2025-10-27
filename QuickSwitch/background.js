5eb773e138c5646830d20d287038bcf2ebd01cbb
Shrekich69 <leniviesosochki@yandex.ru>
2025-10-28 01:35:29 +0500

    Расширение епт для браузера

---
+++ QuickSwitch/background.js
@@ -0,0 +1,53 @@
+chrome.commands.onCommand.addListener( (command) => {
+    switch (command) {
+        case "next_tab":
+            switchToNextTab();
+            break;
+        case "prev_tab":
+            switchToPrevTab();
+            break;
+    }
+})
+
+function getCurrentTab(tabsArray) {
+    let currentTab;
+    for (const element of tabsArray) {
+        if (element.active === true) {
+            currentTab = element;
+            break;
+        }
+    }
+    return currentTab;
+}
+
+function switchToNextTab() {
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+        const currentTab = getCurrentTab(tabs);
+        
+        const currentIndex = tabs.findIndex(tab => {
+            if (tab.id === currentTab.id) {
+                return tab;
+            }
+        });
+        const nextIndex = currentIndex + 1;
+
+        chrome.tabs.update(tabs[nextIndex].id, { active: true });
+        console.log("Switched to the next tab");
+    });
+}
+
+function switchToPrevTab() {
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+        const currentTab = getCurrentTab(tabs);
+        
+        const currentIndex = tabs.findIndex(tab => {
+            if (tab.id === currentTab.id) {
+                return tab;
+            }
+        });
+        const prevIndex = (currentIndex - 1 < 0) ? 0 : currentIndex - 1;
+
+        chrome.tabs.update(tabs[prevIndex].id, { active: true });
+        console.log("Switched to the previous tab");
+    });
+}
